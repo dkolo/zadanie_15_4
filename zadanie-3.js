3171e971c7c86d985d1b63786574e3beed221f74
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-07-12 12:08:03 +0200

    Change let variable name from "logAllArguments" to "average"

--- zadanie-3.js
+++ zadanie-3.js
@@ -1,5 +1,5 @@
 let sum = 0;
-let logAllArguments = (...args) => {args.forEach(arg => sum += arg);
+let average = (...args) => {args.forEach(arg => sum += arg);
 
 console.log(`Suma argumentów wynosi ${sum}`);
 console.log(`Średnia podanych argumentów wynosi ${sum/args.length}`);
