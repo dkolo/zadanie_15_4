c4f1aa5a19aa729f903bf6a7a47110c44affcf3f
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-07-12 12:04:24 +0200

    Add console.log which shows average of all given arguments

--- zadanie-3.js
+++ zadanie-3.js
@@ -2,4 +2,5 @@ let sum = 0;
 let logAllArguments = (...args) => {args.forEach(arg => sum += arg);
 
 console.log(`Suma argumentów wynosi ${sum}`);
+console.log(`Średnia podanych argumentów wynosi ${sum}/${args.length}`);
 }
